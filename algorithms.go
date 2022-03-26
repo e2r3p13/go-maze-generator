8d59485cbf7ec86f2da52d0f2fcf6428ad1f7fa9
Loic Buckwell <bccyv@protonmail.com>
2022-03-26 10:26:57 +0100

    Implement binary tree algoritm, and modify Algorithm interface

--- algorithms.go
+++ algorithms.go
@@ -1,8 +1,13 @@
 package go_maze_generator
 
+import "math/rand"
+
 type Algorithm interface {
-	on(*Grid)
-	step_on(*Grid)
+	init_for(*Grid)
+	is_initialized() bool
+	is_over() bool
+	perform()
+	perform_step()
 }
 
 // ----------------
@@ -21,14 +26,46 @@ func (s Sidewinder) step_on(grid *Grid) {
 
 // -----------------
 
-type BinaryTree struct {
-	current_cell Cell
+type binaryTree struct {
+	grid *Grid
+	current_cell_index int
 }
 
-func (s BinaryTree) on(grid *Grid) {
-
+func New_BinaryTree() *binaryTree {
+	bt := &binaryTree {}
+	return bt
 }
 
-func (s BinaryTree) step_on(grid *Grid) {
+func (b *binaryTree) init_for(grid *Grid) {
+	b.grid = grid
+	b.current_cell_index = 0 
+}
 
+func (b *binaryTree) is_initialized() bool {
+	return b.grid != nil
+}
+
+func (b *binaryTree) is_over() bool {
+	return b.current_cell_index == b.grid.Size()
+}
+
+func (b *binaryTree) perform() {
+	for !b.is_over() {
+		b.perform_step()
+	}
+}
+
+func (b *binaryTree) perform_step() {
+	cell := b.grid.At_index(b.current_cell_index)	
+	dests := make([]*Cell, 0, 2)
+	
+	if cell.n.dest != nil { dests = append(dests, cell.n.dest) }
+	if cell.e.dest != nil { dests = append(dests, cell.e.dest) }
+
+	if len(dests) > 0 {
+		choice := rand.Intn(len(dests))	
+		cell.Link(dests[choice], true)
+	}
+
+	b.current_cell_index += 1
 }
